173e5ec1a31ceec27f4aef71875af8f34e78a29a
Oleh Pona <lanuoleg@gmail.com>
2023-11-01 22:47:14 +0200

    homework 7

--- HomeWork-7/6/script.js
+++ HomeWork-7/6/script.js
@@ -1,15 +1,4 @@
-
-const data = {
-    "german":["Berlin","Hamburg","Munich "],
-    "usa":["New York","Los Angeles","Chicago"],
-    "ukraine":["Kyiv","Lviv","Odesa"]
-}
-for (let i of data[document.getElementById("countries").value]) {
-    document.getElementById("city").innerHTML += `<option>${i}</option>`
-}
-document.getElementById("countries").addEventListener("change", function(){
-    document.getElementById("city").innerHTML = '';
-    for (let i of data[document.getElementById("countries").value]) {
-        document.getElementById("city").innerHTML += `<option>${i}</option>`
-    }
+document.getElementById("a").innerText = `Width: ${window.innerWidth}, Height: ${window.innerHeight}`
+window.addEventListener("resize",()=>{
+    document.getElementById("a").innerText = `Width: ${window.innerWidth}, Height: ${window.innerHeight}`
 })
